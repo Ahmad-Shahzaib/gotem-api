35109c39466312c36101282ac3c912c8f0fb0d02
Munawer Anwar <44869991+Ahmad-Shahzaib@users.noreply.github.com>
2022-08-25 03:03:12 +0500

    update

--- database/db.js
+++ database/db.js
@@ -1,13 +1,16 @@
 const { Sequelize } = require('sequelize');
 const logger = require('../utils/logger');
-console.log('PORT', process.env.DB_PORT);
 module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
 	dialect: 'postgres',
 	dialectOptions: {
 		charset: 'utf8',
 	},
+	ssl: {
+		rejectUnauthorized: true,
+	},
 	host: process.env.DB_HOST,
 	port: process.env.DB_PORT,
-	logging: (msg) => (process.env.NODE_ENV !== 'production' ? logger.info(msg) : false),
+	logging: (msg) => logger.info(msg),
+	debug: false,
 	// logging: process.env.NODE_ENV !== 'production',
 });
